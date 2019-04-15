3ffa7ff6713e289f2c2c9dd39b6888e2b607166f
Benita Britto <34784181+benitakbritto@users.noreply.github.com>
2019-04-15 10:19:19 +0530

    Add files via upload

--- complainList.js
+++ complainList.js
@@ -3,13 +3,13 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
-
+const jsdom = require("jsdom");
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '',
 	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	database : 'complaints'
+	database : '?????'
 });
 
 var app = express();
@@ -30,32 +30,33 @@ app.post('/', function(request, response) {
 		connection.query('SELECT * FROM complaints', function(error, results, fields) {
 						for(i=0; i<fields.length; i++)
 						{
-							//////make changes here based on complaints db
-							var divArray = [document.createElement("div"),
-							document.createElement("div"),
-							document.createElement("div"),
-							document.createElement("div"),
-							document.createElement("div"),
-							document.createElement("div"),
-							document.createElement("div"),
-							document.createElement("div"),
-							document.createElement("div"),
-							document.createElement("div"),];
-
-							document.body.appendChild(divArray);
-
-							divArray[0].innerHTML = response.send(results[i].c_id);
-							divArray[1].innerHTML = response.send(results[i].user_id_by);
-							divArray[2].innerHTML = response.send(results[i].cc_id);
-							divArray[3].innerHTML = response.send(results[i].c_description);
-							divArray[4].innerHTML = response.send(results[i].complaint_date);
-							divArray[5].innerHTML = response.send(results[i].complaint_allotment_date);
-							divArray[6].innerHTML = response.send(results[i].complaint_completed_date);
-							divArray[7].innerHTML = response.send(results[i].complaint_status);
-							divArray[8].innerHTML = response.send(results[i].deleted);
-							divArray[9].innerHTML = response.send(results[i].user_id_alloted_to);
+							const dom = new JSDOM(`<body>
+  												<script> var parent = document.createElement("div");
+  												document.body.appendChild(parent);
+  												var divArray = [document.createElement("div"),
+																document.createElement("div"),
+																document.createElement("div"),
+																document.createElement("div"),
+																document.createElement("div"),
+																document.createElement("div"),
+																document.createElement("div"),
+																document.createElement("div"),
+																document.createElement("div"),
+																document.createElement("div"),];
+												parent.appendChild(divArray);
+												divArray[0].innerHTML = response.send(results[i].c_id);
+												divArray[1].innerHTML = response.send(results[i].user_id_by);
+												divArray[2].innerHTML = response.send(results[i].cc_id);
+												divArray[3].innerHTML = response.send(results[i].c_description);
+												divArray[4].innerHTML = response.send(results[i].complaint_date);
+												divArray[5].innerHTML = response.send(results[i].complaint_allotment_date);
+												divArray[6].innerHTML = response.send(results[i].complaint_completed_date);
+												divArray[7].innerHTML = response.send(results[i].complaint_status);
+												divArray[8].innerHTML = response.send(results[i].deleted);
+												divArray[9].innerHTML = response.send(results[i].user_id_alloted_to);
+  												</script>
+												</body>`);
 						}
-						
 						console.log("successful");
 						response.end();
 				});
